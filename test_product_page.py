2ecb3f52f5a7c43b7c193155616a9301bfb66f7f
kate <berlioz458@gmail.com>
2019-06-28 15:54:00 +0700

    add markup for test

--- test_product_page.py
+++ test_product_page.py
@@ -10,7 +10,7 @@ from .pages.product_page import ProductPage
 class TestUserAddToCartFromProductPage(object):
     @pytest.fixture(scope="function", autouse="True")
     def setup(self, browser):
-        link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+        link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
         login_page = LoginPage(browser, link)
         login_page.open()
         email = str(time.time()) + "@gmo.com"
@@ -18,34 +18,25 @@ class TestUserAddToCartFromProductPage(object):
         login_page.register_new_user(email, password)
         login_page.should_be_authorized_user()
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_cart(self, browser):
-        link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/"
+        link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/"
         page = ProductPage(browser, link)
         page.open()  # открыли страницу
-        page.add_cart_to_pocket()  # добавили товар в корзину и решили уравнение
+        page.add_cart_to_pocket()  # добавили товар в корзину
         page.should_be_message_about_add()  # проверили наименование товара
         page.should_be_message_about_price()  # проверели цену товара
 
     def test_user_cant_see_success_message(self, browser):
-        link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/"
+        link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/"
         page = ProductPage(browser, link)
         page.open()
         page.should_not_be_success_message()
 
 
-@pytest.mark.skip
-@pytest.mark.parametrize('links', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
-                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
-                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
-                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
-                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
-                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
-                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
-                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
-                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
-def test_guest_can_add_product_to_cart(browser, links):
-    link = "{}".format(links)
+@pytest.mark.need_review
+def test_guest_can_add_product_to_cart(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/"
     page = ProductPage(browser, link)
     page.open()  # открыли страницу
     page.add_cart_to_pocket()  # добавили товар в корзину и решили уравнение
@@ -54,19 +45,20 @@ def test_guest_can_add_product_to_cart(browser, links):
 
 
 def test_guest_cant_see_success_message(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/"
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/"
     page = ProductPage(browser, link)
     page.open()
     page.should_not_be_success_message()
 
 
 def test_guest_should_see_login_link_on_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/"
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/"
     page = ProductPage(browser, link)
     page.open()
     page.should_be_login_link()
 
 
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_cart_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/"
     page = ProductPage(browser, link)
@@ -77,8 +69,9 @@ def test_guest_cant_see_product_in_cart_opened_from_product_page(browser):
     basket_page.should_be_text_about_empty()
 
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/"
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/"
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
