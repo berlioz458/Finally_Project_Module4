b7920e0e597974d80aba4476ce7de0f792457c9f
kate <berlioz458@gmail.com>
2019-06-28 15:32:35 +0700

    add new method for success register pricheska :)

--- pages/base_page.py
+++ pages/base_page.py
@@ -13,8 +13,22 @@ class BasePage(object):
         self.url = url
         self.browser.implicitly_wait(timeout)
 
-    def open(self):
-        self.browser.get(self.url)
+    def go_to_basket(self):
+        link_basket = self.browser.find_element(*MainPageLocators.BASKET_LINK)
+        link_basket.click()
+
+    def go_to_login_page(self):
+        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        link.click()
+
+    #  is_disappeared: будет ждать до тех пор, пока элемент не исчезнет.
+    def is_disappeared(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout, 1, TimeoutException). \
+                until_not(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return False
+        return True
 
     def is_element_present(self, how, what):
         try:
@@ -32,15 +46,8 @@ class BasePage(object):
 
         return False
 
-    #  is_disappeared: будет ждать до тех пор, пока элемент не исчезнет.
-    def is_disappeared(self, how, what, timeout=4):
-        try:
-            WebDriverWait(self.browser, timeout, 1, TimeoutException). \
-                until_not(EC.presence_of_element_located((how, what)))
-        except TimeoutException:
-            return False
-
-        return True
+    def open(self):
+        self.browser.get(self.url)
 
     def solve_quiz_and_get_code(self):
         alert = self.browser.switch_to.alert
@@ -55,13 +62,9 @@ class BasePage(object):
         # except NoAlertPresentException:
         #   print("No second alert presented")
 
-    def go_to_login_page(self):
-        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
-        link.click()
+    def should_be_authorized_user(self):
+        assert self.is_element_present(*BasePageLocators.USER_ICON), "User icon is not presented," \
+                                                                     " probably unauthorised user"
 
     def should_be_login_link(self):
         assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
-
-    def go_to_basket(self):
-        link_basket = self.browser.find_element(*MainPageLocators.BASKET_LINK)
-        link_basket.click()
